bd0b040960de4b99d35670c06dbc20894af6daaf
Harsh <guptaharsh768@gmail.com>
2020-08-21 23:52:14 +0530

    added view file for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req,res){     // giving a name to the controller for '/' as home as module.exports is an object 
-    return res.end('<h1>Express is up for codeial</h1>')
-};
+    return res.render('home',{
+        title : "home"
+    });
+}
 
 //format = module.exports.address_name = function(req,res){}
