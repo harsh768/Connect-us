ac2de3314deb01bbe85e40f8cfaaa8eca26c0c98
Harsh <guptaharsh768@gmail.com>
2020-08-24 08:19:40 +0530

    added partials, layouts and set up the statuc files

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express=require('express');
 const app=express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+//accessing static files
+app.use(express.static('assests'));
+
+app.use(expressLayouts);
+
+//extract style and scripts from sub pages into layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 //use express router
 app.use('/',require('./routes'));
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err)
