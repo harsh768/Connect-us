dc9a73aa1ba313c0090663360e0cefafb19740d0
Harsh <guptaharsh768@gmail.com>
2020-08-27 11:38:46 +0530

    Used manual authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,13 @@
 module.exports.home = function(req,res){     // giving a name to the controller for '/' as home as module.exports is an object 
+    
+    console.log(req.cookies);
+    res.cookie('user_id',25);
+
     return res.render('home',{
         title : "home"
     });
 }
+
+
 
 //format = module.exports.address_name = function(req,res){}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,48 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res)
 {
     return res.end('<h1>User profile</h1>');
+}
+
+//rendering the singup page
+module.exports.signup = function(req,res){     
+    return res.render('user_sign_up',{
+        title : "Welcome to signup page of Codeial"
+    });
+}
+
+//rendering the signin page
+module.exports.signin = function(req,res){     
+    return res.render('user_sign_in',{
+        title : "Welcome to signin page of Codeial"
+    });
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
+
+
+//Signin and create a session for the user
+module.exports.createSession = function(req,res){
+    //TODO Later
 }
