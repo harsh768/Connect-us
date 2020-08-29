765ca033d2c7b57f04e0bf6ba1c7d601e1aa2e5a
Harsh <guptaharsh768@gmail.com>
2020-08-29 12:04:02 +0530

    added sass and start database relations

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 
+//Using SASS
+const sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware
+    ({
+        src : './assets/scss',
+        dest : './assets/css',
+        debug : true,
+        outputStyle : 'extended',
+        prefix : '/css'
+    }))
+
+
 //Middlewares
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
 //accessing static files
-app.use(express.static('assests'));
+app.use(express.static('assets'));
 
 app.use(expressLayouts);
 
