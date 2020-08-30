0d853450508bee98843342081ad11e5994e695dc
Harsh <guptaharsh768@gmail.com>
2020-08-30 10:15:47 +0530

    display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,27 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){     // giving a name to the controller for '/' as home as module.exports is an object 
-    
-    console.log(req.cookies);
-    res.cookie('user_id',25);
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
 
-    return res.render('home',{
-        title : "home"
-    });
+    // Post.find({}, function(err,posts)
+    // {
+    //     return res.render('home',{
+    //         title : "Codeial | home",
+    //         posts : posts
+    //     });
+    // })
+
+    //Populating the user of each post
+    Post.find({}).populate('user').exec(function(err,posts)
+    {
+        return res.render('home',{
+            title : "Codeial | home",
+            posts : posts
+        });
+    })
+
+
 }
-
-
 
 //format = module.exports.address_name = function(req,res){}
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ console.log('router loaded');
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 router.use('/harsh',require('./harsh_routers'));
 
 module.exports = router; 
